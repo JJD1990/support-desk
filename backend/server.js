73530302b9329f6e913f581363f006f2c20b417e
jjd1990 <jack.douglas@rocketmail.com>
2022-05-18 21:30:02 +0100

    db added and connected, errorhandler added, async express added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log('Server...')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const connectDB = require('./config/db')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const PORT = process.env.PORT || 3000;
+
+// connect to db
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.send('hello');
+})
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
