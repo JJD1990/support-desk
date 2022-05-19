80d521db0b217754102519acb5f9895e8e598c2c
jjd1990 <jack.douglas@rocketmail.com>
2022-05-19 15:31:57 +0100

    file structure and ports sorted for frontend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 const colors = require('colors');
 const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // connect to db
 
