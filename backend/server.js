e588195c9e9415e93706b0253881ecbc530b2673
Jack <91466216+JJD1990@users.noreply.github.com>
2023-02-27 09:38:02 +0000

    update app.listen
    added app.listen(process.env.PORT || 5000)); to server.js

--- backend/server.js
+++ backend/server.js
@@ -40,4 +40,6 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(process.env.PORT || 5000));
+
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
